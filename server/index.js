2904b56d2c8c6babd96a1625796b219f26bb2971
Hossein <hossein.tb@gmail.com>
2020-12-12 00:52:12 -0500

    updated comment

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// Priority serve any static files.
+// Priority serve any static files
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
