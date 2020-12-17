25a8bfa27c92dda7c2bb7fa59a2e5ae61eca6071
Hossein <hossein.tb@gmail.com>
2020-12-16 23:49:02 -0500

    removed unneccessary js files

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -2,48 +2,52 @@ import React, { Component } from "react";
 import "./Portfolio.scss";
 
 const getMedia = (project) => {
-  if (project.videoLink) {
-    return (
-      <iframe
-        title="Contact Tracing App"
-        style={{ minHeight: "227px" }}
-        src={project.videoLink}
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    );
-  } else {
-    const projectImage = "images/portfolio/thumbnails/" + project.image;
-    return <img alt={project.title} src={projectImage} />;
-  }
+	if (project.videoLink) {
+		return (
+			<iframe
+				title="Contact Tracing App"
+				style={{ minHeight: "227px" }}
+				src={project.videoLink}
+				frameBorder="0"
+				allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+				allowFullScreen
+			></iframe>
+		);
+	} else {
+		const projectImage = "images/portfolio/thumbnails/" + project.image;
+		return <img alt={project.title} src={projectImage} />;
+	}
 };
 
-const getProjects = (project) => <li key={project._id}>
-      <div className="timeline-content">
-        {/* <h3 className="date">{project.date}</h3> */}
-        <h1>{project.title}</h1>
-        {getMedia(project)}
-        <p>{project.description}</p>
-        {project.url && (
-          <p>
-            Click <a href={project.url} target="_blank">here</a> for more details
-          </p>
-        )}
-      </div>
-    </li>
+const getProjects = (project) => (
+	<li key={project._id}>
+		<div className="timeline-content">
+			{/* <h3 className="date">{project.date}</h3> */}
+			<h1>{project.title}</h1>
+			{getMedia(project)}
+			<p>{project.description}</p>
+			{project.url && (
+				<p>
+					Click{" "}
+					<a href={project.url} target="_blank">
+						here
+					</a>{" "}
+					for more details
+				</p>
+			)}
+		</div>
+	</li>
+);
 
 const Portfolio = ({ data }) => {
-  return (
-    <section id="portfolio" className="portfolio-container">
-      <h1 className="portfolio__header"> Check Out Some of My Works </h1>
-      <div className="timeline">
-        <ul>
-          {data && data.map((project) => getProjects(project))}
-        </ul>
-      </div>
-    </section>
-  );
-}
+	return (
+		<section id="portfolio" className="portfolio-container">
+			<h1 className="portfolio__header"> Check Out Some of My Works </h1>
+			<div className="timeline">
+				<ul>{data && data.map((project) => getProjects(project))}</ul>
+			</div>
+		</section>
+	);
+};
 
 export default Portfolio;
