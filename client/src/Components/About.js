cdd141708fa82e3bb03a502725e37c14b1828dc4
Hossein <hossein.tb@gmail.com>
2020-12-23 12:44:48 -0500

    added proptypes for about section

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,30 +1,35 @@
-import React, { Component } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 import "./About.scss";
 
-class About extends Component {
-  render() {
-    if(this.props.data){
-       var bio = this.props.data[0];
-      var profilepic= "images/"+bio.image;
-      var bio1 = bio.bio1;
-      var bio2 = bio.bio2;
-    }
+const About = props => {
+   const bio = props.data[0];
+   const profilePic= "images/"+bio.image;
+   const about = bio.about;
 
-    return (
+   return (
       <section id="about">
          <div className="row">
             <div className="three columns">
-               <img className="profile-pic"  src={profilepic} alt="Hossein Torabi Profile Pic" />
+               <img className="profile-pic"  src={profilePic} alt="Hossein Torabi Profile Pic" />
             </div>
             <div className="nine columns main-col">
                <h2>About Me</h2>
-               <p>{bio1}</p>
-               <p>{bio2}</p>
+               {about.map((item, index) => <p key={index}>{item}</p>)}
             </div>
          </div>
       </section>
-    );
-  }
+   );
+}
+
+About.propTypes = {
+   data: PropTypes.arrayOf(
+      PropTypes.shape({
+            image: PropTypes.string.isRequired,
+            about: PropTypes.arrayOf(
+               PropTypes.string.isRequired
+            )
+      }))
 }
 
 export default About;
--- client/src/Components/Loader.js
+++ client/src/Components/Loader.js
@@ -1,20 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { loadingStates } from "../constants";
 import "./Loader.scss";
 
 const Loader = ({ loadingStatus }) => {
 
     return <header id="home" className="header--loader">
-        <div class="loader__container">
+        <div className="loader__container">
             {loadingStatus === loadingStates.LOADING ?
-            <><svg class="loader__icon"viewBox="25 25 50 50" >
-                <circle class="loader__path" cx="50" cy="50" r="20" fill="none" stroke-width="2" />
+            <><svg className="loader__icon"viewBox="25 25 50 50" >
+                <circle className="loader__path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" />
             </svg>
             <h1 className="loader__text">Loading ...</h1></>: 
             <h1 className="loader__text">Sorry! <br /> The content cannot be loaded. <br /> Please try again later </h1>
             }
         </div>
     </header>
+}
+
+Loader.propTypes = {
+   loadingStatus: PropTypes.oneOf([
+       loadingStates.LOADING,
+       loadingStates.LOADED,
+       loadingStates.ERROR
+    ]) 
 }
 
 export default Loader;
