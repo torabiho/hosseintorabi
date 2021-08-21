259a60ce0a5e42e664509f21bd7316b233172a70
torabiho <hossein.tb@gmail.com>
2021-08-21 14:25:54 -0400

    Allow-Origin Header

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ const root = path.join(__dirname, '../client', 'build');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+  });
 app.use(express.static(root)); // Priority serve any static files
 app.use(subdomain('blog.api', blogRouter)); // Handle all requests from the blog
 app.use("/api",portfolioRouter); // Handle all requests from the main site
