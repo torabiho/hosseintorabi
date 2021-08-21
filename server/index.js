b885e4a75cc6695b6370cfbc8205edca8aa472d9
torabiho <hossein.tb@gmail.com>
2021-08-21 12:21:45 -0400

    test subdomain

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import path from "path";
 import mongoose from "mongoose";
 import expressWs from 'express-ws';
-import subdomain from "express-subdomain";
+import vhost from "vhost";
 import { dbConnectionInfo } from "../config.js";
 import portfolioRouter from "./portfolio/portfolioRouter";
 import blogRouter from "./blog/blogRouter";
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(root)); // Priority serve any static files
-app.use(subdomain('blog', blogRouter)); // Handle all requests from the blog
+app.use(vhost('blog.hosseintorabi.com', blogRouter));
+//app.use(subdomain('blog', blogRouter)); // Handle all requests from the blog
 //app.use("/api",portfolioRouter); // Handle all requests from the main site
 
 // All remaining requests return the React app, so it can handle routing.
