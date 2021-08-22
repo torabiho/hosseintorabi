592582f922e59a1efc5f434f8d5d0df12bc9f4e0
torabiho <hossein.tb@gmail.com>
2021-08-22 02:19:25 -0400

    set accept language

--- server/blog/models/post.js
+++ server/blog/models/post.js
@@ -1,20 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import { dbConnectionInfo } from "../../../config.js";
 
 const blogDB = mongoose.connection.useDb(dbConnectionInfo.dbs.blog);
+const translationSchema = {
+  title: { type: String, required: true },
+  subtitle: { type: String },
+  content: [
+    {
+      main: String,
+    },
+  ],
+};
 const PostSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    title: { type: String, required: true },
-    subtitle: { type: String },
-    postDate: { type: Date, required: true },
-    content: [{
-        main:String,
-    }],
-    visible: { type: Boolean, required: true },
+  _id: mongoose.Schema.Types.ObjectId,
+  postDate: { type: Date, required: true },
+  visible: { type: Boolean, required: true },
+  en: translationSchema,
+  fa: translationSchema,
 });
 
-
-
-export default blogDB.model('Post', PostSchema);
-
-
+export default blogDB.model("Post", PostSchema);
