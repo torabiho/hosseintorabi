bed434c6919a1f0af4ccbe9c4abd5136c8a6fb28
connected-htorabi <htorabi@connected.io>
2021-08-29 22:05:55 -0400

    test cors

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(cors());
 // });
 app.use(express.static(root)); // Priority serve any static files
 app.use(subdomain("blog.api", blogRouter)); // Handle all requests from the blog
-app.use(subdomain("www", mainRouter)); // Handle all requests from the main site
-app.use(cors());
+app.use("/api", mainRouter);
+// app.use(subdomain("www", mainRouter)); // Handle all requests from the main site
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("*", (req, res) => {
