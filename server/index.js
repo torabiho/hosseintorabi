87e8c879136a679a1225317e3125e5570a4eed61
connected-htorabi <htorabi@connected.io>
2021-08-29 22:28:43 -0400

    set main site router

--- server/index.js
+++ server/index.js
@@ -11,21 +11,15 @@ import blogRouter from "./blog/blogRouter";
 const port = process.env.PORT || 50080;
 const app = express();
 const mainRouter = express.Router();
-const ws = expressWs(app);
+// const ws = expressWs(app);
 const root = path.join(__dirname, "../client", "build");
 
-//mainRouter.use("/api", portfolioRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
 app.use(express.static(root)); // Priority serve any static files
 app.use(subdomain("blog.api", blogRouter)); // Handle all requests from the blog
 app.use("/api", portfolioRouter);
-// app.use(subdomain("www", mainRouter)); // Handle all requests from the main site
 
 // All remaining requests return the React app, so it can handle routing.
 app.get("*", (req, res) => {
