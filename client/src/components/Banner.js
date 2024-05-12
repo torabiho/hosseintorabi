8e1f106972202ffbbec35c20ebb9e49ef1310015
torabiho <hossein.tb@gmail.com>
2024-05-11 18:09:04 -0700

    updated node modules

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -2,43 +2,51 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Banner.scss";
 
-const getBannerInfo = bio =>{
-    return <>
-        <h1 className="banner__header">I'm {bio.name}.</h1>
-        <h3>I'm a {bio.address.city} based <span>{bio.occupation}</span>. {bio.description}</h3>
-        <hr />
-        <ul className="social">
-            {bio.social.map(network => <li key={network.name}>
-                    <a href={network.url} target='_blank'>
-                        <i className={`fa fa-${network.name}-square`}></i>
-                    </a>
-                </li>
-            )}
-        </ul>
+const getBannerInfo = (bio) => {
+  return (
+    <>
+      <h1 className="banner__header">I'm {bio.name}.</h1>
+      <h3>
+        I'm a {bio.address.city} based <span>{bio.occupation}</span>.{" "}
+        {bio.description}
+      </h3>
+      <hr />
+      <ul className="social">
+        {bio.social.map((network) => (
+          <li key={network.name}>
+            <a href={network.url} target="_blank">
+              <i className={`fa fa-${network.name}-square`}></i>
+            </a>
+          </li>
+        ))}
+      </ul>
     </>
-}
+  );
+};
 
 export const Banner = ({ data }) => {
-    return <div className="row banner">
-        <div className="banner__container">
-            {getBannerInfo(data[0])}
-        </div>
+  return (
+    <div className="row banner">
+      <div className="banner__container">{getBannerInfo(data[0])}</div>
     </div>
-}
+  );
+};
 
 Banner.propTypes = {
-   data: PropTypes.arrayOf(
-      PropTypes.shape({
-            address: PropTypes.shape({
-              city: PropTypes.string,
-            }),
-            description: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            occupation: PropTypes.string.isRequired,
-            social: PropTypes.arrayOf(PropTypes.shape({
-                name : PropTypes.string,
-                url: PropTypes.string
-               })
-            )
-      })).isRequired
-}
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      address: PropTypes.shape({
+        city: PropTypes.string,
+      }),
+      description: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      occupation: PropTypes.string.isRequired,
+      social: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string,
+          url: PropTypes.string,
+        })
+      ),
+    })
+  ).isRequired,
+};
--- server/index.js
+++ server/index.js
@@ -25,10 +25,7 @@ app.get("*", (req, res) => {
   res.sendFile("index.html", { root });
 });
 
-mongoose.connect(dbConnectionInfo.dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(dbConnectionInfo.dbUrl);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
